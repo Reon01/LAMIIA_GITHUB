98781f8b70995f6b7e16f959570fc5f9c5d17d8f
Reon0712 <m0a212121d@edu.teu.ac.jp>
2023-05-21 11:48:18 +0900

    更新

--- Assets/Reon/PlayerMove3.cs
+++ Assets/Reon/PlayerMove3.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerMove3 : MonoBehaviour
 {
-void Update()
+    public CinemachineBrain brain;
+    private ICinemachineCamera Cam;
+    private CinemachineVirtualCamera vcam;
+    public float kando;
+    private GameObject vCamObj;
+
+    void Start()
     {
+        GameObject vCamObj = GameObject.Find("GameObject");
+        vcam = vCamObj.GetComponent<CinemachineVirtualCamera>();
+    }
+
+    void Update()
+    {
+        var pov = vcam.GetCinemachineComponent<CinemachinePOV>();
+        pov.m_VerticalAxis.m_MaxSpeed = kando;
+        pov.m_HorizontalAxis.m_MaxSpeed = kando;
         // カメラの向きからY軸の回転を計算する
         Quaternion horizontalRot = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
 
@@ -14,5 +30,18 @@ void Update()
 
         // プレイヤーの回転を更新する
         transform.rotation = horizontalRot * verticalRot;
-    }
+
+
+        //従来のカメラ操作
+/*
+// カメラの向きからY軸の回転を計算する
+Quaternion horizontalRot = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
+
+// カメラの向きからX軸とZ軸の回転を計算する
+Quaternion verticalRot = Quaternion.Euler(Camera.main.transform.eulerAngles.x, 0f, Camera.main.transform.eulerAngles.z);
+
+// プレイヤーの回転を更新する
+transform.rotation = horizontalRot * verticalRot;
+*/
+}
 }
