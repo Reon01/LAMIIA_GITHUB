56cfd4e1f446e7c8691300de84c7ff8a9cb49c55
T-Haruma <mg4.m1911@icloud.com>
2023-06-04 00:02:05 +0900

    Tutorial_BGMの実装

--- Assets/Haruma/AutoLoader/MSAL.cs
+++ Assets/Haruma/AutoLoader/MSAL.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ManagerSceneAutoLoader
+public class MSAL
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void LoadManagerScene()
