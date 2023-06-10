9c8a13e9649073bd09f566fdec416b5eed8c1ef6
masmn <m0a2113856@edu.teu.ac.jp>
2023-06-10 11:42:55 +0900

    optionとBossの動き

---
+++ Assets/Masamune_Profiles/BossMove.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMove : MonoBehaviour
+{
+    //[SerializeField] private GameObject player;
+    [SerializeField] private GameObject WhaleGO1;
+    [SerializeField] private GameObject WhaleGO2;
+    [SerializeField] private GameObject WhaleGO3;
+    [SerializeField] private GameObject WhaleGO4;
+    //[SerializeField] GameObject circlingtarget;
+    //public float TurnSpan = 10f;
+    //private float currentTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //フィールド上周回
+        transform.LookAt(WhaleGO1.transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("WhaleGO1"))
+        {
+            transform.LookAt(WhaleGO2.transform);
+
+        }
+        if (other.CompareTag("WhaleGO2"))
+        {
+            transform.LookAt(WhaleGO3.transform);
+
+        }
+        if (other.CompareTag("WhaleGO3"))
+        {
+            transform.LookAt(WhaleGO4.transform);
+
+        }
+        if (other.CompareTag("WhaleGO4"))
+        {
+            transform.LookAt(WhaleGO1.transform);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.Translate(0, 0, 10.0f * Time.deltaTime);
+
+        //主人公に突進
+        /*currentTime += Time.deltaTime;
+        if ((currentTime > TurnSpan) && (Time.timeScale == 1))
+        {
+            transform.LookAt(player.transform);
+            currentTime = 0f;
+        }
+        if (Time.timeScale == 1)
+        {
+            transform.Translate(0, 0, 1.3f);
+        }
+        */
+
+        //ステージ周回
+        //transform.RotateAround(/*クジラの周回の中心になるもの*/circlingtarget.transform.position, Vector3.up, 50 * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/Masamune_Profiles/OptionAble.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionAble : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()//つけてるオブジェクトがアクティブなとき
+    {
+        Time.timeScale = 0;
+    }
+    private void OnDisable()//アクティブじゃないとき
+    {
+        Time.timeScale = 1;
+    }
+}
