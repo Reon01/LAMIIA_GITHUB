61d620f695296a9d77a3d7ce52e9a664b76cd15d
Reon0712 <m0a212121d@edu.teu.ac.jp>
2023-07-07 16:44:22 +0900

    チュートリアルの修正、バグ修正

---
+++ Assets/Scene2_Tutorial/Scripts/Canvas_Purpose/Tips/Tip9.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tip9 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Scene3_Tutorial skillcharge bossbattle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
