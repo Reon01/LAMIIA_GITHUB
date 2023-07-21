fbe626fcf0d28f980d1999e37e42386296f3a7d7
Reon0712 <m0a212121d@edu.teu.ac.jp>
2023-07-21 19:45:19 +0900

    ダメージテキスト関連

--- Assets/Scene4_BossBattle/Scripts/S4_BossHP.cs
+++ Assets/Scene4_BossBattle/Scripts/S4_BossHP.cs
@@ -10,6 +10,13 @@ public class S4_BossHP : MonoBehaviour
     public GameObject gameclearsystem;
     public GameObject bosssystems;
 
+    //↓ダメージ表記用
+    public GameObject damage10;
+    public GameObject damage20;
+    public GameObject damage50;
+    public float damagecooltime;
+    public GameObject damagetextposition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +40,7 @@ public class S4_BossHP : MonoBehaviour
         {
             hp -= 10;
             slider_bosshp.value = hp;
+            damagedisplay10(); //１０ダメージのテキストを表示
         }
 
         //↓カジキの場合50DMG
@@ -40,6 +48,23 @@ public class S4_BossHP : MonoBehaviour
         {
             hp -= 50;
             slider_bosshp.value = hp;
+            damagedisplay50();　//５０ダメージのテキストを表示
         }
+    }
+
+    //１０ダメージのテキストを表示
+    public void damagedisplay10()
+    {
+        Instantiate(damage10, damagetextposition.transform.position, Quaternion.identity);
+    }
+    //２０ダメージのテキストを表示
+    public void damagedisplay20()
+    {
+        Instantiate(damage20, damagetextposition.transform.position, Quaternion.identity);
+    }
+    //５０ダメージのテキストを表示
+    public void damagedisplay50()
+    {
+        Instantiate(damage50, damagetextposition.transform.position, Quaternion.identity);
     }
 }
