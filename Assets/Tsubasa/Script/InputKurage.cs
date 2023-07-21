001f0d025787180f4ddc5259f83b40b6914c7aeb
TUT03 <m0a210493c@edu.teu.ac.jp>
2023-07-21 15:11:40 +0900

    コントローラー入力用のスキルスクリプト作成

---
+++ Assets/Tsubasa/Script/InputKurage.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputKurage : MonoBehaviour
+{
+    public GameObject kuragesield, player;
+    public int kuragesieldHP;
+    public bool isSkill;
+
+    //はるまサウンド用
+    public static bool Kurage_Sound_s = false;
+    public static bool Kurage_Sound_e = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kuragesield.SetActive(false);
+        kuragesieldHP = 0;
+        isSkill = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+
+    public void SkillKurage()
+    {
+        if (kuragesield.activeSelf == false && isSkill == true &&
+            GetComponent<GetSkill>().a_Kurage >= 1)
+        {
+            kuragesield.SetActive(true);
+            kuragesieldHP = 1;
+            player.GetComponent<PlayerHP>().kaihuku();
+            GetComponent<GetSkill>().a_Kurage -= 1; //スキルを１消費
+
+            //はるまサウンド用変数true
+            Kurage_Sound_s = true;
+        }
+
+        if (kuragesieldHP <= 0)
+        {
+            kuragesield.SetActive(false);
+            //Debug.Log("クラゲ消滅");
+        }
+    }
+
+    public void damage()
+    {
+        //kuragesieldHP -= 1;
+        kuragesield.SetActive(false);
+        Debug.Log("クラゲ消滅");
+        Kurage_Sound_e = true;
+    }
+
+}
