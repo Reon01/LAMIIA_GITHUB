86d83f4d22fc92616e7a57843b06bd53aa0914fb
Reon0712 <m0a212121d@edu.teu.ac.jp>
2023-09-12 16:40:49 +0900

    ・英語設定を実装（現在シーン２の最初とシーン４のみ＝まだ途中）

---
+++ Assets/Scene2_Tutorial/Scripts/TextController_EN.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController_EN : MonoBehaviour
+{
+    public Text textLabel;
+    public string charaName;
+    public string[] word;
+    private int num = 0;
+
+    public GameObject panel;
+    public GameObject tip;
+    public GameObject HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textLabel.text = charaName + "\n" + word[num];
+        Time.timeScale = 0;
+
+        //サウンド用
+        SFXplayer.radio_Sound = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(1) || 
+            Input.GetKeyDown(KeyCode.Space))
+        {
+            //サウンド用
+            SFXplayer.radio_Sound_obs = true;
+
+            if (num == word.Length - 1)
+            {
+                //サウンド用
+                SFXplayer.radio_Sound = 3;
+                
+                this.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                panel.SetActive(true);
+                tip.SetActive(true);
+                HP.SetActive(true);
+
+                return;
+            }
+
+            num += 1;
+
+            textLabel.text = charaName + "\n" + word[num];
+        }
+    }
+}
